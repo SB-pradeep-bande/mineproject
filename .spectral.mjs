290dc90137b0a5193ea7a544a1509398381f7578
pradeep.bande@smartbear.com <pradeep.bande@smartbear.com>
2024-08-22 17:18:48 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1NjM4Nw/spectral.js?branch_slug=main&token=3bd9d346-7082-43e4-a1b7-87c4d43cbc95";
+export default { extends: ruleset };
